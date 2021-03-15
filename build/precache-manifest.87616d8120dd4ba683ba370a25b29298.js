0bfdfcef74a03b17e0d79eaa7a96d5386742f199
unknown <huber.guevara@gimsaber.edu.co>
2021-03-15 17:41:58 -0500

    subiendo repositorio

---
+++ build/precache-manifest.87616d8120dd4ba683ba370a25b29298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d263457a348eb288509ab91fd0a77115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc61ad4daf4ef3add67",
+    "url": "/static/css/main.2959cba5.chunk.css"
+  },
+  {
+    "revision": "3b0fb8cf4a165c4b5ecb",
+    "url": "/static/js/2.88f13635.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.88f13635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc61ad4daf4ef3add67",
+    "url": "/static/js/main.5ec2ed5d.chunk.js"
+  },
+  {
+    "revision": "6765d93c93b0aa16e2ff",
+    "url": "/static/js/runtime-main.24e2082f.js"
+  }
+]);
